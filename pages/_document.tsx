2fbeabe25b83596b9166533e03666b5cedc03ebc
Tong Yin <tongyin002@gmail.com>
2022-05-22 23:49:22 -0700

    extend _document

---
+++ pages/_document.tsx
@@ -0,0 +1,68 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="google" content="notranslate" />
+          <meta name="theme-color" content="#1976D2" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Muli:300,400:latin&display=optional"
+            rel="stylesheet"
+          />
+          <link
+            rel="shortcut icon"
+            href="https://storage.googleapis.com/builderbook/favicon32.png"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+          <link
+            rel="stylesheet"
+            href="https://storage.googleapis.com/builderbook/nprogress.min.css"
+          />
+          <link
+            rel="stylesheet"
+            href="https://storage.googleapis.com/builderbook/vs.min.css"
+          />
+
+          <style>
+            {`
+             a {
+                font-weight: 400;
+                color: #58a6ff;
+                outline: none;
+              }
+              blockquote {
+                padding: 0 1em;
+                color: #555;
+                border-left: 0.25em solid #dfe2e5;
+              }
+              pre {
+                display: block;
+                overflow-x: auto;
+                padding: 0.5em;
+                background: #FFF;
+                border: 1px solid #ddd;
+              }
+              code {
+                font-size: 14px;
+                background: #FFF;
+                padding: 3px 5px;
+              }
+            `}
+          </style>
+        </Head>
+        <body>
+          <Main></Main>
+          <NextScript></NextScript>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
